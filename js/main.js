1472fcbc3c896270484afdbf8c0d21f68795ae5b
LuanX <101584126+chienthan-03@users.noreply.github.com>
2024-07-07 23:37:56 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
     // process bar
     setTimeout(function() {
-        firstQuestion();
         $('.spinner').fadeOut();
         $('#preloader').delay(350).fadeOut('slow');
         $('body').delay(350).css({
